2030287929c50d8ef336a1daaa052cec4faad270
lifan <1406433326@qq.com>
2022-01-17 09:44:59 +0800

    刚新建好文件夹，0.1版本的文件夹架构

---
+++ cmake-build-debug/CMakeFiles/VoCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VoCode.dir/main.cpp.o"
+  "VoCode"
+  "VoCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VoCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/VoCode.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for VoCode.
+# This may be replaced when dependencies are built.
